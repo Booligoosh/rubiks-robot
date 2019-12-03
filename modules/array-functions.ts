8b32d2fcb58af49ec1730a052e0ed0a1e5586b2b
Ethan <me@ethan.link>
2019-12-03 23:58:32 +1100

    Removed some of the warnings from array-functions

--- modules/array-functions.ts
+++ modules/array-functions.ts
@@ -1,8 +1,9 @@
-export function removeDuplicates (array: any[]): any[] {
+export function removeDuplicates (array: unknown[]): any[] {
     // https://stackoverflow.com/questions/9229645/remove-duplicate-values-from-js-array/9229821#9229821
     return Array.from(new Set(array))
 }
 
-export function removeDuplicateArrays (arrayOfArrays: any[][]): any[][] {
-    return removeDuplicates(arrayOfArrays.map(array => JSON.stringify(array.sort()))).map(array => JSON.parse(array))
+export function removeDuplicateArrays (arrayOfArrays: unknown[][]): any[][] {
+    return removeDuplicates(arrayOfArrays.map((array: unknown[]) => JSON.stringify(array.sort())))
+        .map((array: string) => JSON.parse(array))
 }
