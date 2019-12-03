c32ca85c171dc2444a5c74ad8410623e1a4ca866
Ethan <me@ethan.link>
2019-12-03 23:16:29 +1100

    Got CV stuff to pass ESLint

--- cube-detection.js
+++ cube-detection.js
@@ -1,12 +1,10 @@
-import {WHITE, GREEN, RED, YELLOW, BLUE, ORANGE, COLORS} from './modules/constants';
-import {getNameForColor} from './modules/color-functions';
-import {getMaskRangesForColor, getImageMaskForColor} from './modules/cv-functions';
-import sleep from './modules/sleep';
-import * as cv from 'opencv4nodejs';
+import {WHITE, GREEN, RED, YELLOW, BLUE, ORANGE, COLORS} from './modules/constants'
+import {getNameForColor} from './modules/color-functions'
+import {getMaskRangesForColor, getImageMaskForColor} from './modules/cv-functions'
+import sleep from './modules/sleep'
+import * as cv from 'opencv4nodejs'
 
-readCube();
-
-async function readCube() {
+async function readCube () {
 
     // Webcamera no 0 is used to capture the frames 
     //let cap = new cv.VideoCapture(0);
@@ -14,21 +12,23 @@ async function readCube() {
     //await sleep(1000); // https://stackoverflow.com/q/28566972/
 
     //let frame = cap.read();
-    let frame = cv.imread('./cube-test.jpg');
+    const frame = cv.imread(`./cube-test.jpg`)
     // Converts image from BGR to HSV
-    let hsv = frame.cvtColor(cv.COLOR_BGR2HSV);
+    const hsv = frame.cvtColor(cv.COLOR_BGR2HSV)
     
-    cv.imshowWait('original', frame);
+    cv.imshowWait(`original`, frame)
     
-    for(let color of COLORS) {
-        if(typeof getMaskRangesForColor(color) !== 'undefined') {
-            cv.imshowWait(getNameForColor(color), getImageMaskForColor(hsv, color));
+    for(const color of COLORS) {
+        if(typeof getMaskRangesForColor(color) !== `undefined`) {
+            cv.imshowWait(getNameForColor(color), getImageMaskForColor(hsv, color))
         }
     }
 
     // Destroys all of the HighGUI windows. 
-    cv.destroyAllWindows();
+    cv.destroyAllWindows()
 
     // release the captured frame 
     //cap.release();
 }
+
+readCube()
--- modules/cv-functions.js
+++ modules/cv-functions.js
@@ -1,50 +1,20 @@
-import {WHITE, GREEN, RED, YELLOW, BLUE, ORANGE, COLORS} from './constants';
-import * as cv from 'opencv4nodejs';
+import {WHITE, GREEN, RED, YELLOW, BLUE, ORANGE, COLORS} from './constants'
+import * as cv from 'opencv4nodejs'
 
-export function getImageMaskForColor(image, color) {
-    return getImageMaskForRanges(image, getMaskRangesForColor(color));
-}
-
-export function getImageMaskForRanges(image, ranges) {
-    // Ranges looks like this: [ { lower: [1,2,3], upper: [1,2,3] } ]
-    let mask = null;
-    for (let range of ranges) {
-        let maskToAdd = getImageMaskForRange(image, range.lower, range.upper);
-        if (mask == null) {
-            mask = maskToAdd;
-        } else {
-            mask = mask.bitwiseOr(maskToAdd);
-        }
-    }
-    return mask;
-}
-
-export function getImageMaskForRange(image, lower, upper) {
-    return image.inRange(new cv.Vec3(...lower), new cv.Vec3(...upper));
-}
-
-export function multiChannelApplyMask(img, mask) {
-    let channels = img.splitChannels();
-    let maskedChannels = channels.map(c => c.bitwiseAnd(mask));
-    let output = new cv.Mat(maskedChannels);
-
-    return output;
-}
-
-export function getMaskRangesForColor(color) {
-    let table = {};
+export function getMaskRangesForColor (color) {
+    const table = {}
     table[WHITE] = [
         {
             lower: [0, 0, 240],
             upper: [179, 20, 255]
         }
-    ];
+    ]
     table[GREEN] = [
         {
             lower: [41, 50, 50],
             upper: [80, 255, 255]
         }
-    ];
+    ]
     table[RED] = [
         {
             lower: [0, 50, 50],
@@ -54,25 +24,55 @@ export function getMaskRangesForColor(color) {
             lower: [160, 50, 50],
             upper: [179, 255, 255]
         }
-    ];
+    ]
     table[YELLOW] = [
         {
             lower: [20, 50, 50],
             upper: [40, 255, 255]
         }
-    ];
+    ]
     table[BLUE] = [
         {
             lower: [85, 50, 50],
             upper: [130, 255, 255]
         }
-    ];
+    ]
     table[ORANGE] = [
         {
             lower: [6, 50, 50],
             upper: [19, 255, 255]
         }
-    ];
-    
-    return table[color];
+    ]
+
+    return table[color]
+}
+
+export function getImageMaskForRange (image, lower, upper) {
+    return image.inRange(new cv.Vec3(...lower), new cv.Vec3(...upper))
+}
+
+export function getImageMaskForRanges (image, ranges) {
+    // Ranges looks like this: [ { lower: [1,2,3], upper: [1,2,3] } ]
+    let mask = null
+    for (const range of ranges) {
+        const maskToAdd = getImageMaskForRange(image, range.lower, range.upper)
+        if (mask === null) {
+            mask = maskToAdd
+        } else {
+            mask = mask.bitwiseOr(maskToAdd)
+        }
+    }
+    return mask
+}
+
+export function getImageMaskForColor (image, color) {
+    return getImageMaskForRanges(image, getMaskRangesForColor(color))
+}
+
+export function multiChannelApplyMask (img, mask) {
+    const channels = img.splitChannels()
+    const maskedChannels = channels.map(c => c.bitwiseAnd(mask))
+    const output = new cv.Mat(maskedChannels)
+
+    return output
 }
